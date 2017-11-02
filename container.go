087ed298bb982b815280df523fbe904b516c6e32
lvzhuoshi@lmdgame.com <lvzhuoshi@lmdgame.com>
2017-11-02 13:57:14 +0800

    优化
    优化

--- container.go
+++ container.go
@@ -11,7 +11,7 @@ type item struct {
 }
 
 type priorityQueue struct {
-	s  []*item
+	im map[int]int
 	m  map[int]*item
 	cf func(l, r *item) bool
 }
@@ -19,20 +19,20 @@ type priorityQueue struct {
 func minHeapComp(l, r *item) bool { return l.priority < r.priority }
 func maxHeapComp(l, r *item) bool { return l.priority > r.priority }
 
-func (r *priorityQueue) Len() int           { return len(r.s) }
-func (r *priorityQueue) Less(i, j int) bool { return r.cf(r.s[i], r.s[j]) }
+func (r *priorityQueue) Len() int           { return len(r.im) }
+func (r *priorityQueue) Less(i, j int) bool { return r.cf(r.m[r.im[i]], r.m[r.im[j]]) }
 func (r *priorityQueue) Swap(i, j int) {
-	r.s[i], r.s[j] = r.s[j], r.s[i]
-	r.s[i].index = i
-	r.s[j].index = j
+	r.im[i], r.im[j] = r.im[j], r.im[i]
+	r.m[r.im[i]].index = i
+	r.m[r.im[j]].index = j
 }
 
 func (r *priorityQueue) Push(x interface{}) {
 	it := x.(*item)
 	if _, ok := r.m[it.value]; !ok {
-		n := len(r.s)
+		n := len(r.im)
 		it.index = n
-		r.s = append(r.s, it)
+		r.im[n] = it.value
 		r.m[it.value] = it
 	} else {
 		LogWarn("heap can't insert repeated value:%v", it.value)
@@ -40,9 +40,9 @@ func (r *priorityQueue) Push(x interface{}) {
 }
 
 func (r *priorityQueue) Pop() interface{} {
-	n := len(r.s)
-	it := r.s[n-1]
-	r.s = r.s[0 : n-1]
+	n := len(r.im)
+	it := r.m[r.im[n-1]]
+	delete(r.im, n-1)
 	delete(r.m, it.value)
 	return it
 }
@@ -67,8 +67,8 @@ func (r *Heap) Update(value, priority int) {
 }
 
 func (r *Heap) GetMin() (priority int, value int) {
-	n := len(r.p.s)
-	it := r.p.s[n-1]
+	it := heap.Pop(r.p).(*item)
+	heap.Push(r.p, it)
 	return it.priority, it.value
 }
 
@@ -86,13 +86,13 @@ func (r *Heap) Len() int {
 }
 
 func NewMinHeap() *Heap {
-	h := &Heap{p: &priorityQueue{m: map[int]*item{}, cf: minHeapComp}}
+	h := &Heap{p: &priorityQueue{m: map[int]*item{}, im: map[int]int{}, cf: minHeapComp}}
 	heap.Init(h.p)
 	return h
 }
 
 func NewMaxHeap() *Heap {
-	h := &Heap{p: &priorityQueue{m: map[int]*item{}, cf: maxHeapComp}}
+	h := &Heap{p: &priorityQueue{m: map[int]*item{}, im: map[int]int{}, cf: maxHeapComp}}
 	heap.Init(h.p)
 	return h
 }
--- container_test.go
+++ container_test.go
@@ -9,13 +9,44 @@ func Test_MinHeap(t *testing.T) {
 	for i := 10000; i > 0; i-- {
 		mh.Push(i, i)
 	}
+	m, p := mh.GetMin()
 
-	i := mh.Pop()
-	Println(i, mh.Len())
+	Printf("min : %v %v \n", m, p)
+	for i := 0; i < 10; i++ {
+		x := mh.Pop()
+		Printf("%v  ", x)
+	}
+
+	Println("")
 
 	mh.Push(1, 554654)
 	mh.Push(1, 333)
+	i := mh.Pop()
+	Println(i, mh.Len())
+
+	mh.Push(0, 19384)
 	i = mh.Pop()
+	Println(i, mh.Len())
+}
+
+func Test_MaxHeap(t *testing.T) {
+	mh := NewMaxHeap()
+	for i := 10000; i > 0; i-- {
+		mh.Push(i, i)
+	}
+	m, p := mh.GetMin()
+
+	Printf("max : %v %v \n", m, p)
+	for i := 0; i < 10; i++ {
+		x := mh.Pop()
+		Printf("%v  ", x)
+	}
+
+	Println("")
+
+	mh.Push(1, 554654)
+	mh.Push(1, 333)
+	i := mh.Pop()
 	Println(i, mh.Len())
 
 	mh.Push(0, 19384)
