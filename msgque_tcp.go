70f6f57941069510f2188d92823a1eddf61f1326
magiclvzs <441707528@qq.com>
2020-03-04 19:24:52 +0800

    Update msgque_tcp.go

--- msgque_tcp.go
+++ msgque_tcp.go
@@ -379,7 +379,7 @@ func (r *tcpMsgQue) connect() {
 	LogDebug("connect to addr:%s msgque:%d", r.address, r.id)
 	c, err := net.DialTimeout(r.network, r.address, time.Second)
 	if err != nil {
-		LogDebug("connect to addr:%s failed msgque:%d err:%v", r.address, r.id, err)
+		LogError("connect to addr:%s failed msgque:%d err:%v", r.address, r.id, err)
 		r.handler.OnConnectComplete(r, false)
 		atomic.CompareAndSwapInt32(&r.connecting, 1, 0)
 		r.Stop()
