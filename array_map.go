ade26b0b7b5060152084ac489c0c80d0f9a5fe4a
magiclvzs <441707528@qq.com>
2020-06-28 16:13:03 +0800

    Update array_map.go

--- array_map.go
+++ array_map.go
@@ -1,24 +1,26 @@
 package antnet
 
-import (
-	"sync"
-	"sync/atomic"
-)
-
-type arrayMap struct {
-	rawCap   int
-	safe     bool
+type ArrayMap struct {
+	rawCap   int32
 	genArray []interface{}
 	delArray []int32
-	sync.Mutex
 	genIndex int32
 	delIndex int32
 }
 
-func NewArrayMap(cap int, threadSafe bool) *arrayMap {
-	return &arrayMap{
+func (r *ArrayMap) Clone() *ArrayMap {
+	return &ArrayMap{
+		rawCap:   r.rawCap,
+		genArray: append([]interface{}{}, r.genArray...),
+		delArray: append([]int32{}, r.delArray...),
+		genIndex: r.genIndex,
+		delIndex: r.delIndex,
+	}
+}
+
+func NewArrayMap(cap int32) *ArrayMap {
+	return &ArrayMap{
 		rawCap:   cap,
-		safe:     threadSafe,
 		genArray: make([]interface{}, cap),
 		delArray: make([]int32, cap),
 		genIndex: -1,
@@ -26,63 +28,7 @@ func NewArrayMap(cap int, threadSafe bool) *arrayMap {
 	}
 }
 
-func (r *arrayMap) addSafe(value interface{}) int32 {
-	var id int32 = -1
-	for r.delIndex > 0 && id == -1 {
-		ov := r.delIndex
-		nv := r.delIndex - 1
-		if nv >= 0 {
-			if atomic.CompareAndSwapInt32(&r.delIndex, ov, nv) {
-				id = r.delArray[ov]
-			}
-		}
-	}
-	if id == -1 {
-		id = atomic.AddInt32(&r.genIndex, 1)
-		if id >= int32(len(r.genArray)) {
-			r.Lock()
-			if id >= int32(len(r.genArray)) {
-				newArray := make([]interface{}, r.rawCap)
-				r.genArray = append(r.genArray, newArray...)
-				newDelArray := make([]int32, r.rawCap)
-				r.delArray = append(r.delArray, newDelArray...)
-			}
-			r.Unlock()
-		}
-	}
-
-	r.genArray[id] = value
-	return id
-}
-
-func (r *arrayMap) delSafe(index int32) {
-	id := atomic.AddInt32(&r.delIndex, 1)
-	r.delArray[id] = index
-}
-
-func (r *arrayMap) Add(value interface{}) int32 {
-	if r.safe {
-		return r.addSafe(value)
-	}
-	return r.add(value)
-}
-
-func (r *arrayMap) Del(index int32) {
-	if r.safe {
-		r.delSafe(index)
-	} else {
-		r.del(index)
-	}
-}
-func (r *arrayMap) Len() int32 {
-	return r.genIndex - r.delIndex
-}
-
-func (r *arrayMap) Get(index int32) interface{} {
-	return r.genArray[index]
-}
-
-func (r *arrayMap) add(value interface{}) int32 {
+func (r *ArrayMap) add(value interface{}) int32 {
 	var id int32 = -1
 	if r.delIndex > 0 {
 		id = r.delArray[r.delIndex]
@@ -102,8 +48,44 @@ func (r *arrayMap) add(value interface{}) int32 {
 	return id
 }
 
-func (r *arrayMap) del(index int32) {
+func (r *ArrayMap) del(index int32) {
 	id := r.delIndex + 1
 	r.delArray[id] = index
+	r.genArray[index] = nil
 	r.delIndex++
+}
+
+func (r *ArrayMap) set(key int32, value interface{}) {
+	for key >= int32(len(r.genArray)) {
+		newArray := make([]interface{}, r.rawCap)
+		r.genArray = append(r.genArray, newArray...)
+		newDelArray := make([]int32, r.rawCap)
+		r.delArray = append(r.delArray, newDelArray...)
+	}
+	r.genArray[key] = value
+}
+
+func (r *ArrayMap) Add(value interface{}) int32 {
+	return r.add(value)
+}
+
+func (r *ArrayMap) Set(key int32, value interface{}) {
+	r.set(key, value)
+}
+
+func (r *ArrayMap) Del(index int32) {
+	r.del(index)
+}
+func (r *ArrayMap) Len() int32 {
+	return r.genIndex - r.delIndex
+}
+func (r *ArrayMap) RawLen() int32 {
+	return int32(len(r.genArray))
+}
+
+func (r *ArrayMap) Get(index int32) interface{} {
+	if index >= int32(len(r.genArray)) {
+		return nil
+	}
+	return r.genArray[index]
 }
